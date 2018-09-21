88a0e5ad86580cc7b96f30c1f1250e2452237f5d
Virginia Dooley <vcdooley@gmail.com>
2018-09-21 10:46:02 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -32,7 +32,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for (1..7) in number_of_times do
+  for 1..7 in number_of_times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
