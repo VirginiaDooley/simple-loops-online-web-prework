288e6c1873014f24cfc9bb09b888e9f301ce7852
Virginia Dooley <vcdooley@gmail.com>
2018-09-21 10:41:38 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -18,7 +18,7 @@ end
 def while_iterator(number_of_times)
   number_of_times = 0
   number_of_times += 1 
-  while number_of_times <= 7
+  while number_of_times < 7
   puts "Welcome to Flatiron School's Web Development Course!"
 end
 number_of_times
